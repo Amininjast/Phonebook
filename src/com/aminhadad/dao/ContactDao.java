605a6b6b19916ee4bb184e2fd58e8c8f4bcd9077
Amin <aminarad228@gmail.com>
2022-07-25 08:47:36 +0300

    1401-05-03 takmil e tavabe dao

--- src/com/aminhadad/dao/ContactDao.java
+++ src/com/aminhadad/dao/ContactDao.java
@@ -12,10 +12,30 @@ public class ContactDao {
         createStatement.execute(createQuery);
     }
 
-    public void dropDataBase(){}
-    public void update(){}
-    public void insert(){}
-    public void delete(){}
+    public void dropDataBase() throws SQLException {
+        String dropQuery="drop table contact ";
+        Statement dropStatement= H2JDBCUtils.getConnection().createStatement();
+        dropStatement.execute(dropQuery);
+    }
+
+    public void update(int id) throws SQLException {
+        String updateQuery="delete from contact where id=? ";
+        Statement updateStatement= H2JDBCUtils.getConnection().updateStatement();
+        updateStatement.execute(updateQuery);
+    }
+
+    public void insert() throws SQLException {
+        String insertQuery="INSERT INTO contact VALUES (?,?,?) ";
+        Statement insertStatement= H2JDBCUtils.getConnection().insertStatement();
+        insertStatement.execute(insertQuery);
+    }
+
+    public void delete(int id) throws SQLException {
+        String deleteQuery="DELETE FROM contact where id=? ";
+        Statement deleteStatement= H2JDBCUtils.getConnection().deleteStatement();
+        deleteStatement.execute(deleteQuery);
+    }
+
     public Contact selectById(int id) throws SQLException {
         String selectByIdQuery="select * from contact as c where c.id=?";
         Statement selectByIdStatement= H2JDBCUtils.getConnection().createStatement();
